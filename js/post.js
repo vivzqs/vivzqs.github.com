16f681d13d54cf628b9261b0e9c9a791f91c8661
vivzqs <vivzqs@gmail.com>
2013-03-31 09:50:08 +0800

    Update post.js

--- js/post.js
+++ js/post.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     $('pre').addClass('prettyprint linenums') //添加Google code Hight需要的class
     
     window.disqus_shortname = 'vivzqs'; // required: replace example with your forum shortname
-    $('#disqus_thread .comment').on('click',function(){
+    $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
